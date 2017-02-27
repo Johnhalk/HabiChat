ad2eab15ab8636f21029d9a9e4b042c5e2926d14
Ekaterina Loshchinina <KateLoschinina@Ekaterinas-MacBook-Pro-2.local>
2017-02-27 17:09:45 +0000

    Adding feature tests for User Story Two: As a Group Member, so that I can discuss tasks, I would like to be able to comment on the task.

---
+++ spec/features/task_comments_feature_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+feature '#TaskComments' do
+  # User Story Two
+  # As a Group Member,
+  # So that I can discuss tasks,
+  # I would like to be able to comment on the task
+  context 'Adding a comments to a task:' do
+    scenario 'as a signed up user, I can add a comment to a task and see it on the page' do
+      # sign_up
+      visit tasks_path
+      fill_in 'Description', with: 'Wash dishes'
+      check 'Super urgent'
+      click_button 'Add task'
+      fill_in 'Comment', with: 'Test comment'
+      click_button 'Add comment'
+      expect(page).to have_content 'Test comment'
+    end
+    # scenario 'as a not signed up person I can not add a comment to a task' do
+    #   visit tasks_path
+    #   expect(page).not_to have_button 'Add comment'
+    # end
+  end
+end
