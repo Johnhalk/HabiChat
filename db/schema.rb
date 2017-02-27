2b546117c06054feaf2166810a5d45fd451ab189
John Ashton <John_ashton@hotmail.com>
2017-02-27 18:02:48 +0000

    Added comments model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170227174509) do
+ActiveRecord::Schema.define(version: 20170227180126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "comments"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.text     "description"
