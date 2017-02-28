eea55a21ca073c32fe76233ce1a5d6f5756541c2
John Ashton <John_ashton@hotmail.com>
2017-02-28 16:45:27 +0000

    Integrated index styling with calendar

--- db/schema.rb
+++ db/schema.rb
@@ -15,11 +15,6 @@ ActiveRecord::Schema.define(version: 20170228143301) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "calendars", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "comments", force: :cascade do |t|
     t.text     "comments"
     t.datetime "created_at", null: false
