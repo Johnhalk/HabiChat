2734d748f97d0beb4bd039e47cf865cf1636a074
Ekaterina Loshchinina <KateLoschinina@Ekaterinas-MacBook-Pro-6.local>
2017-03-02 10:23:06 +0000

    Adjusting old tests.

--- spec/test_helper.rb
+++ spec/test_helper.rb
@@ -1,12 +1,9 @@
 def sign_up
-  save_and_open_page
-
   visit '/'
-  save_and_open_page
   click_link 'Sign up'
+  fill_in 'Username', with: 'Test Name'
   fill_in 'Email', with: 'test@example.com'
   fill_in 'Password', with: 'testtest'
   fill_in 'Password confirmation', with: 'testtest'
   click_button 'Sign up'
-
 end
