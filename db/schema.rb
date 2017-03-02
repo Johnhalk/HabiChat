d68ee5eee32a44d01fc13eb09783d622dac3932b
John Ashton <John_ashton@hotmail.com>
2017-03-02 17:29:45 +0000

    Added colour to calendar depending on time and made it so tasks on calendars display only time and not date and time

--- db/schema.rb
+++ db/schema.rb
@@ -15,11 +15,6 @@ ActiveRecord::Schema.define(version: 20170301112528) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "calendars", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "chats", force: :cascade do |t|
     t.string   "topic"
     t.datetime "created_at", null: false
@@ -80,6 +75,7 @@ ActiveRecord::Schema.define(version: 20170301112528) do
     t.string   "username"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.index ["username"], name: "index_users_on_username", unique: true, using: :btree
   end
 
   add_foreign_key "comments", "tasks"
