31d3c46198f18808746d9add79c436b85c661499
Tamar Lehmann <tlehmann@hotmail.co.uk>
2017-03-03 12:08:41 +0000

    Undone changes as didn't work

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -1,15 +1,15 @@
 App.messages = App.cable.subscriptions.create('MessagesChannel', {
   received: function(data) {
     $("#messages").removeClass('hidden');
-    $("#messages").addClass('chat-left');
     return $('#messages').append(this.renderMessage(data));
   },
 
   renderMessage: function(data) {
-    if (data.user == current_user) {
-      return "<p class='chat-right'> <b>" + data.user + ": </b>" + data.message + "</p>"; }
-    else {
-      return "<p class='chat-left'> <b>" + data.user + ": </b>" + data.message + "</p>";
-      }
+    // if (data.user == current_user) {
+      return "<p class='chat-right'> <b>" + data.user + ": </b>" + data.message + "</p>";
+  //   }
+  //   else {
+  //     return "<p class='chat-left'> <b>" + data.user + ": </b>" + data.message + "</p>";
+  //     }
   }
 });
