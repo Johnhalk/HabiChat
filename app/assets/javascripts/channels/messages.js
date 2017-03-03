a5b2949074284f1b404a6356f7bc3472365d9490
Tamar Lehmann <tlehmann@hotmail.co.uk>
2017-03-03 11:52:51 +0000

    Styling messages box

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -1,10 +1,11 @@
 App.messages = App.cable.subscriptions.create('MessagesChannel', {
   received: function(data) {
     $("#messages").removeClass('hidden');
+    $("#messages").addClass('chat-left');
     return $('#messages').append(this.renderMessage(data));
   },
 
   renderMessage: function(data) {
-    return "<p> <b>" + data.user + ": </b>" + data.message + "</p>";
+    return "<p class='chat-right'> <b>" + data.user + ": </b>" + data.message + "</p>";
   }
 });
